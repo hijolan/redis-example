83abfd3d44ad3f188f2557d05b570a36100d33bf
Constantin Lebrecht <constantin.lebrecht@gmx.net>
2012-03-01 09:01:27 +0100

    init import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedisExample::Application.config.secret_token = 'b6a7e5f25512662f00465b49c186963ac40287a87d6d9ecfe565baae61a3bcf174705752f0645323f2e4ca3d908fffde88ef7008834b5f0c8eb2f6b7bb144077'
